191edf27abdbbfcbd0c6cb8d352e327964dfdc7a
chromezh <chromezh@outlook.com>
2019-01-29 14:20:24 +0800

    Revisit code

--- files_index/index.js
+++ files_index/index.js
@@ -1,25 +1,28 @@
 "use strict";
 
 function makeArticle(path, fileName, title) {
-    return "<article>"
-    + "<h3><a href=\"" + path + fileName + ".html\">" + title + "</a></h3>"
-    + "</article>";
+    return '<article>'
+    + '<details>'
+    + '<summary>' + title + '</summary>'
+    + '<iframe src="' + path + fileName + '.html"></iframe>'
+    + '</details>'
+    + '</article>';
 }
 
 fetch('/api/dictum')
 .then(function(response) { return response.text(); })
 .then(function(str) {
-    document.getElementById("header_dictum").innerText = str;
+    document.getElementById('header_dictum').innerText = str;
 });
 
 fetch('/api/username')
 .then(function(response) { return response.json(); })
 .then(function(res) {
     if (!res) {
-        document.getElementById("h3_userinfo").innerHTML = "<a href=\"/auth/login\">Log In</a>";
+        document.getElementById('h3_userinfo').innerHTML = '<a href="/auth/login">Log In</a>';
     } else {
-        document.getElementById("contents_useronly").style.display = "inherit";
-        document.getElementById("h3_userinfo").innerHTML = "Welcome, " + res + ". <a href=\"/auth/logout\">Log Out</a>";
+        document.getElementById('contents_useronly').style.display = 'inherit';
+        document.getElementById('h3_userinfo').innerHTML = 'Welcome, ' + res + '. <a href="/auth/logout">Log Out</a>';
     }
 });
 
@@ -27,16 +30,16 @@ fetch('/api/isadmin')
 .then(function(response) { return response.json(); })
 .then(function(res) {
     if (res) {
-        document.getElementById("contents_adminonly").style.display = "inherit";
+        document.getElementById('contents_adminonly').style.display = 'inherit';
 
         fetch('/api/kakitsubata/index.csv')
         .then(function(response) { return response.text(); })
         .then(function(str) {
-            document.getElementById("contents_adminonly").innerHTML += str
-            .split('\n')
+            document.getElementById('contents_adminonly').innerHTML += str
+            .split("\n")
             .map(function (x) { return x.split(','); })
             .filter(function (x) { return x.length == 2; })
-            .map(function (x) { return makeArticle("/api/kakitsubata/", x[0], x[1]); })
+            .map(function (x) { return makeArticle('/api/kakitsubata/', x[0], x[1]); })
             .join('');
         });
     }
@@ -45,10 +48,10 @@ fetch('/api/isadmin')
 fetch('pure/offprint/index.csv')
 .then(function(response) { return response.text(); })
 .then(function(str) {
-    document.getElementById("contents_pubdyn_offprint").innerHTML += str
-    .split('\n')
+    document.getElementById('contents_pubdyn_offprint').innerHTML += str
+    .split("\n")
     .map(function (x) { return x.split(','); })
     .filter(function (x) { return x.length == 2; })
-    .map(function (x) { return makeArticle("pure/offprint/", x[0], x[1]); })
+    .map(function (x) { return makeArticle('pure/offprint/', x[0], x[1]); })
     .join('');
 });
