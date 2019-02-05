4b96cef28447393230ba81cbec77fd04e04f6538
chromezh <chromezh@outlook.com>
2019-02-05 22:55:03 +0900

    Update for Kanji change

--- kanji/index.js
+++ kanji/index.js
@@ -1,33 +1,31 @@
 "use strict";
 
-function makeLi(str) { return '<li>' + str + '</li>'; }
+function makeLi(o) { return '<li>' + o.char + ': ' + o.yomikata + '</li>'; }
 
 function makeList(xs) { return '<ul>' + xs.map(makeLi).join('') + '</ul>'; }
 
-function queryKunyomi() {
-    var wrd = document.getElementById('textInput').value;
-
-    fetch('/api/kunyomi?q=' + wrd)
-    .then(function(response) { return response.json(); })
+function queryKunyomi(ch) {
+    fetch('/api/kunyomi?q=' + ch)
+    .then(function(response) {
+        if (!response.ok) { throw response; }
+        return response.json();
+    })
     .then(function(res) {
-        if (res) {
-            document.getElementById('kunyomi_title').style.display = 'inherit';
-            document.getElementById('kunyomi_res').innerHTML = makeList(res);
-        } else {
-            document.getElementById('kunyomi_title').display = 'none';
-            document.getElementById('kunyomi_res').innerHTML = '';
-        }
+        document.getElementById('kunyomi').innerHTML = res.length ? makeList(res) : '<p>No result</p>';
+    }).catch(function(response) {
+        if (response.status == 400)
+            document.getElementById('kunyomi').innerHTML = '<p>Invalid query character</p>';
     });
 
-    fetch('/api/hanja?q=' + wrd)
-    .then(function(response) { return response.json(); })
+    fetch('/api/hanja?q=' + ch)
+    .then(function(response) {
+        if (!response.ok) { throw response; }
+        return response.json();
+    })
     .then(function(res) {
-        if (res) {
-            document.getElementById('hanja_title').style.display = 'inherit';
-            document.getElementById('hanja_res').innerHTML = makeList(res);
-        } else {
-            document.getElementById('hanja_title').style.display = 'none';
-            document.getElementById('hanja_res').innerHTML = '';
-        }
+        document.getElementById('hanja').innerHTML = res.length ? makeList(res) : '<p>No result</p>';
+    }).catch(function(response) {
+        if (response.status == 400)
+            document.getElementById('hanja').innerHTML = '<p>Invalid query character</p>';
     });
 }
