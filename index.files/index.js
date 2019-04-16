aaf003635b6e3ecbe17ff6696dc5d8e873367f35
sgalal <chromezh@outlook.com>
2019-04-16 23:38:45 +0800

    Use async in index

--- index.files/index.js
+++ index.files/index.js
@@ -1,57 +1,67 @@
-"use strict";
+'use strict';
 
-function makeArticle(path, fileName, title) {
-    return '<article>'
-    + '<h3><a href="' + path + fileName + '.html">' + title + '</a></h3>'
-    + '</article>';
-}
+const makeArticle = (path, fileName, title) => `<article>
+	<h3><a href="${path + fileName}.html">${title}</a></h3>
+</article>`;
 
-function handleArticleCsv(str, path) {
-    return str
-    .split("\n")
-    .map(function (x) { return x.split(','); })
-    .filter(function (x) { return x.length == 2; })
-    .map(function (x) { return makeArticle(path, x[0], x[1]); })
-    .join('');
-}
+const handleArticleCsv = (str, path) => str
+	.split('\n')
+	.map(x => x.split(','))
+	.filter(x => x.length == 2)
+	.map(x => makeArticle(path, x[0], x[1]))
+	.join('');
 
-fetch('/api/dictum')
-.then(function(response) {
-    if (!response.ok) { throw response; }
-    return response.text();
-})
-.then(function(str) {
-    document.getElementById('dictum').innerText = str;
-})
-.catch(function(response) {});
+(async () => {
+	const response = await fetch('/api/dictum');
+	if (!response.ok) {
+		return;
+	} else {
+		const str = await response.text();
+		document.getElementById('dictum').innerText = str;
+	}
+})();
 
-fetch('/api/isuser')
-.then(function(response) {
-    if (!response.ok) { throw response; }
-    return response.text();
-})
-.then(function(res) {
-    document.getElementById('contents_useronly').style.display = 'inherit';
-    document.getElementById('h3_userinfo').innerHTML = 'Welcome, ' + res + '. <a href="/api/auth/logout">Log Out</a>';
-})
-.catch(function(response) {
-    if (response.status == 401)
-        document.getElementById('h3_userinfo').innerHTML = '<a href="/api/auth/page/gitlab/forward">Log In via GitLab</a>';
-});
+(async () => {
+	const response = await fetch('/api/isuser');
+	if (!response.ok) {
+		if (response.status == 401)
+			document.getElementById('h3_userinfo').innerHTML = '<a href="/api/auth/page/gitlab/forward">Log In via GitLab</a>';
+	} else {
+		const res = await response.text();
+		document.getElementById('contents_useronly').style.display = 'inherit';
+		document.getElementById('h3_userinfo').innerHTML = `Welcome, ${res}. <a href="/api/auth/logout">Log Out</a>`;
+	}
+})();
 
-fetch('/o/index.csv')
-.then(function(response) { return response.text(); })
-.then(function(str) {
-    document.getElementById('contents_pubdyn_offprint').innerHTML += handleArticleCsv(str, '/o/');
-});
+(async () => {
+	const response = await fetch('/o/index.csv');
+	if (!response.ok) {
+		return;
+	} else {
+		const str = await response.text();
+		document.getElementById('contents_pubdyn_offprint').innerHTML += handleArticleCsv(str, '/o/');
+	}
+})();
 
-fetch('/p/kakitsubata/index.csv')
-.then(function(response) {
-    if (!response.ok) { throw response; }
-    return response.text();
-})
-.then(function(str) {
-    document.getElementById('contents_adminonly').style.display = 'inherit';
-    document.getElementById('contents_adminonly').innerHTML += handleArticleCsv(str, '/p/kakitsubata/');
-})
-.catch(function(response) {});
+const adminArticlesLoaded = (async () => {
+	const response = await fetch('/p/kakitsubata/index.csv');
+	if (!response.ok) {
+		return;
+	} else {
+		const str = await response.text();
+		document.getElementById('contents_adminonly').style.display = 'inherit';
+		document.getElementById('contents_adminonly').innerHTML += handleArticleCsv(str, '/p/kakitsubata/');
+	}
+})();
+
+(async () => {
+	const preloadHtmlSegmentLoaded = fetch('/p/preload.txt');
+	await adminArticlesLoaded;
+	const response = await preloadHtmlSegmentLoaded;
+	if (!response.ok) {
+		return;
+	} else {
+		const str = await response.text();
+		document.getElementById('contents_adminonly').innerHTML += str;
+	}
+})();
