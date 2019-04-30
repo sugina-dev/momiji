1147a7451679683001d5706329252fbae6b4dbe5
sgalal <32255369+chromezh@users.noreply.github.com>
2019-05-01 01:03:33 +0800

    Update index

--- index.files/index.js
+++ index.files/index.js
@@ -11,29 +11,23 @@ const handleArticleCsv = (str, path) => str
 	.map(x => makeArticle(path, x[0], x[1]))
 	.join('');
 
-(async () => {
+const dictumLoaded = (async () => {
 	const response = await fetch('/t/dictum/');
 	if (!response.ok) {
 		return;
 	} else {
-		const str = await response.text();
-		document.getElementById('dictum').innerText = str;
+		document.getElementById('dictum').innerText = await response.text();
 	}
 })();
 
-(async () => {
+const userLoaded = (async () => {
 	const response = await fetch('/api/isuser');
 	if (!response.ok) {
 		if (response.status == 401)
 			document.getElementById('h3_userinfo').innerHTML = '<a href="/api/auth/page/gitlab/forward">Log In via GitLab</a>';
 	} else {
 		const res = await response.text();
-		document.getElementById('contents_useronly').innerHTML = `<section>
-	<h2>User Utilities</h2>
-	<article>
-		<h3><a href="board/message/">Message Board</a></h3>
-	</article>
-</section>`;
+		document.getElementById('contents_useronly').style.display = 'inherit';
 		document.getElementById('h3_userinfo').innerHTML = `Welcome, ${res}. <a href="/api/auth/logout">Log Out</a>`;
 	}
 })();
@@ -44,24 +38,21 @@ const adminArticlesLoaded = (async () => {
 		return;
 	} else {
 		const str = await response.text();
-		document.getElementById('contents_adminonly').innerHTML = `<section>
-	<h2>Admin Utilities</h2>
-	<article>
-		<h3><a href="board/manage/">Message Board Manager</a></h3>
-	</article>
-</section>`;
-		document.getElementById('contents_adminonly').innerHTML += handleArticleCsv(str, '/p/kakitsubata/');
+		document.getElementById('contents_adminonly').style.display = 'inherit';
+		document.getElementById('contents_adminonly').outerHTML += handleArticleCsv(str, '/p/kakitsubata/');
+	}
+})();
+
+const adminLoaded = (async () => {
+	const [_, response] = await Promise.all([adminArticlesLoaded, fetch('/p/preload.txt')]);
+	if (!response.ok) {
+		return;
+	} else {
+		document.getElementById('contents_adminonly').outerHTML += await response.text();
 	}
 })();
 
 (async () => {
-	const preloadHtmlSegmentLoaded = fetch('/p/preload.txt');
-	await adminArticlesLoaded;
-	const response = await preloadHtmlSegmentLoaded;
-	if (!response.ok) {
-		return;
-	} else {
-		const str = await response.text();
-		document.getElementById('contents_adminonly').innerHTML += str;
-	}
-})();
+	await Promise.all([dictumLoaded, userLoaded, adminLoaded]);
+	document.body.style.display = 'inherit';
+})
